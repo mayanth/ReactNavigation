0d96e0368d48ae5a7ecdf43b0e2456ed82363e2b
mayanth <anothai.ssht@gmail.com>
2022-10-10 16:14:17 +0700

    Navigation

---
+++ src/components/page/Home.js
@@ -0,0 +1,12 @@
+import Logo from "../../Logo.jpg"
+
+const Home=()=>{
+    return(
+        <div>
+            <img src={Logo} alt="Logo"/>
+            <h1>หน้าแรก</h1>
+        </div>
+    )
+}
+
+export default Home
